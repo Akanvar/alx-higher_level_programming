eab5a36861821499f75bf20bfc89fc5bb003c04c
Akanvar <kelvinawiapo9@gmail.com>
2023-11-29 01:28:27 +0000

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def upper_case(c):
+    if 96 < ord(c) < 123:
+        return (ord(c) - 32)
+    else:
+        return ord(c)
+
+def uppercase(str):
+    new = ""
+    for c in str:
+        new += "%c" % upper_case(c)
+    print("{:s}".format(new))
