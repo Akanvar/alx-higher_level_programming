854e8f3706f799b9873eb9a70514e18e58fff753
Akanvar <kelvinawiapo9@gmail.com>
2024-02-15 23:38:08 +0000

    force user

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
 /* Write a script that creates the table force_name on your MySQL server.
 force_name description: `id` INT, `name` VARCHAR(256) can’t be null */
 
-USE hbtn_0d_2;
-
 CREATE TABLE IF NOT EXISTS force_name (`id` INT, `name` VARCHAR(256) NOT NULL);
