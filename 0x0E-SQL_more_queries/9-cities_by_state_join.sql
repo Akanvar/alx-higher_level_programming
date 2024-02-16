1969ec6d2561fe9fb1021ee4e9b252f9f55dcf72
Akanvar <kelvinawiapo9@gmail.com>
2024-02-16 01:06:02 +0000

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@
 -- You can use only one SELECT statement
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.id = states.id
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
