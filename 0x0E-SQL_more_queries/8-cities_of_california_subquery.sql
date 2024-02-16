607ff12b47bef7d35d0c781d40889c53f24fb5ec
Akanvar <kelvinawiapo9@gmail.com>
2024-02-16 00:42:35 +0000

    subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,6 @@
 -- that can be found in the database hbtn_0d_usa.
 /*  The states table contains only one record where name = California (but the id can be different, as per the example)
     Results must be sorted in ascending order by cities.id */
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name
 FROM cities
 WHERE cities.state_id = (SELECT id FROM states WHERE name = 'California')
